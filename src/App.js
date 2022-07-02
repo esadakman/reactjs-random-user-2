198610765e016fd025f6ec224b5fdd7dd437d0fe
esadakman <esadd26@gmail.com>
2022-07-02 16:12:36 +0300

    v2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import "./App.css";
-import Navbar from "./components/Navbar/Navbar";
+import './App.css';
+import Navbar from './components/Navbar/Navbar';
+import Footer from "./components/Footer/Footer"
+import PersonInfo from "./components/PersonInfo/PersonInfo"
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
+      <Navbar/>
+      <PersonInfo/>
+      <Footer/>
     </div>
   );
 }
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,14 +1,15 @@
-import styling from "./Navbar.module.scss";
-import cw from "../../assets/cw.svg";
+import React from 'react'
+import styling from "./Navbar.module.scss"
+import cw from "../../assets/cw.svg"
 
 const Navbar = () => {
   return (
     <div className={styling.container}>
-      <nav className={styling.navbar}>
-        <img className={styling.img} src={cw} alt="clarusway" />
-      </nav>
+        <nav className={styling.navbar}>
+            <img src={cw} alt="clarusway" className={styling.image}/>
+        </nav>
     </div>
-  );
-};
+  )
+}
 
-export default Navbar;
+export default Navbar
